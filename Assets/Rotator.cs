46b385d6ab5e7a47cba2303a0f39fbb658b04abd
ntrixner <n.trixner@gmx.at>
2019-08-09 22:05:16 +0200

    Create Turrents, Ninja Model, Cluster Prefab

---
+++ Assets/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * speed);
+    }
+}
