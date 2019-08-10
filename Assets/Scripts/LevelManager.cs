790bbc57aa39b0c4b37aadfaf6a76272e88f5f21
ntrixner <n.trixner@gmx.at>
2019-08-10 15:02:17 +0200

    Main Menu, basic scene handling

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    int currentLevel = 1;
+    int totalLevels = 3;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void Awake()
+    {
+        if(FindObjectsOfType<LevelManager>().Length > 1)
+        {
+            Destroy(this);
+        }
+    }
+
+    public void LevelComplete(int levelNumber)
+    {
+        PlayerPrefs.SetInt("Level", levelNumber);
+    }
+
+    public void LoadLevel(int levelNumber)
+    {
+        currentLevel = levelNumber;
+        SceneManager.LoadScene(levelNumber + 2);
+    }
+
+    public bool HasLevel(int levelNumber)
+    {
+        int levelgotten = PlayerPrefs.GetInt("Level", 0);
+
+        return levelNumber <= levelgotten;
+    }
+
+    public void ShowDeathScreen()
+    {
+        currentLevel = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowSuccessScreen()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LevelDone()
+    {
+        if (totalLevels < currentLevel)
+        {
+            currentLevel += 1;
+            LoadLevel(currentLevel);
+        }
+    }
+}
