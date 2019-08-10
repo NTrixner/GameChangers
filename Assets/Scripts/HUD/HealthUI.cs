59c03ba922a2cd5a44f599f647d638f5b3d67b73
ntrixner <n.trixner@gmx.at>
2019-08-10 12:20:01 +0200

    Add Health UI

---
+++ Assets/Scripts/HUD/HealthUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField]
+    Image empty;
+
+    [SerializeField]
+    Image full;
+
+    public bool isFull
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        isFull = true;
+    }
+    public void SetEmpty()
+    {
+        full.gameObject.SetActive(false);
+        empty.gameObject.SetActive(true);
+        isFull = false;
+    }
+
+    public void SetFull()
+    {
+
+        full.gameObject.SetActive(true);
+        empty.gameObject.SetActive(false);
+        isFull = true;
+    }
+
+}
