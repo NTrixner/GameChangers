b3f0a6f6927ebcb58bcde5a76b4b28720fcdd866
ntrixner <n.trixner@gmx.at>
2019-08-11 12:08:52 +0200

    MainMenu Music + Intro

---
+++ Assets/Scripts/AudioIntro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioIntro : MonoBehaviour
+{
+    // Play an intro Clip followed by a loop
+    [SerializeField]
+    private AudioSource introAudioSource;
+    [SerializeField]
+    private AudioSource loopAudioSource;
+
+    void Start()
+    {
+        double introDuration = (double)introAudioSource.clip.samples / introAudioSource.clip.frequency;
+        double startTime = AudioSettings.dspTime;
+        introAudioSource.PlayScheduled(startTime);
+        loopAudioSource.PlayScheduled(startTime + introDuration);
+    }
+}
